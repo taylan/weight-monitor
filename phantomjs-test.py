537bde078d7b17c5c6dc3e98c817b17c5f396261
Taylan Aydinli <taylanaydinli@gmail.com>
2014-01-10 11:23:36 +0200

    Changed how phantomjs script runs

--- phantomjs-test.py
+++ phantomjs-test.py
@@ -15,9 +15,9 @@ if not phantomjs_path:
     print('No PhantomJS executable defined for platform {0}.'.format(platform))
 exe_path = path.join(path.dirname(path.realpath(__file__)), phantomjs_path)
 print(exe_path)
-print('{0} phantomjs-test.js'.format(exe_path))
+print('{0} {1} '.format(exe_path, path.join(path.dirname(path.realpath(__file__)), 'phantomjs-test.js')))
 
 print('starting')
-pjs_proc = Popen('{0} phantomjs-test.js'.format(exe_path))
+pjs_proc = Popen('{0} {1} '.format(exe_path, path.join(path.dirname(path.realpath(__file__)), 'phantomjs-test.js')))
 pjs_proc.wait()
 print('done')
