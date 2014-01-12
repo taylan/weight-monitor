5b8aa9a58fbc074f5e7a47e46aa2dfb838d6ec49
Taylan Aydinli <taylanaydinli@gmail.com>
2014-01-12 20:31:21 +0200

    Passing destination file name to PhantomJS script
    Also removed vendor directory and renamed phantomjs
    executables.

--- phantomjs-test.js
+++ phantomjs-test.js
@@ -1,5 +1,9 @@
 var page = require('webpage').create();
+var args = require('system').args;
+var destFile = args[1];
+console.log(args);
+console.log(args[1]);
 page.open('http://github.com/', function () {
-    page.render('github.png');
+    page.render(destFile);
     phantom.exit();
 });
--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 from subprocess import Popen, call
-from os import path
+from os import path, rename
 from sys import platform
+from uuid import uuid4
 
 
 
-exe_name = 'phantomjs-{0}'.format(platform)
+exe_name = '_phantomjs-{0}'.format(platform)
 exe_path = path.join(path.dirname(path.realpath(__file__)), exe_name)
 
 call(['chmod', '+x', exe_path])
+dest_file_name = '{0}.png'.format(uuid4())
 
 print('starting')
-params = '{0} {1}'.format(exe_path, 'phantomjs-test.js').split(' ')
+params = '{0} {1} {2}'.format(exe_path, 'phantomjs-test.js', dest_file_name).split(' ')
 print(params)
 pjs_proc = Popen(params)
 pjs_proc.wait()
 print('done')
 
-print('fuhu!' if path.exists('github.png') else 'fuk!')
+
+print('fuhu!' if path.exists(dest_file_name) else 'fuk!')
