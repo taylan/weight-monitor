4f3ec92312e7fbaa282d849410815833942f1401
Taylan Aydinli <taylanaydinli@gmail.com>
2014-01-17 15:18:23 +0200

    Added inline editing functionality on weight list

--- static/js/weight-monitor.js
+++ static/js/weight-monitor.js
@@ -1,4 +1,11 @@
-$(document).ready(function(){
+function saveWeight(u, d) {
+    $.post(u, d)
+        .done(function (data) {
+            console.log('save done', data)
+        });
+}
+
+$(document).ready(function () {
     $('#input-date').pickadate({
         format: 'yyyy-mm-dd',
         formatSubmit: 'yyyy-mm-dd',
@@ -6,11 +13,20 @@ $(document).ready(function(){
         clear: false
     });
 
-    $("#weight-form").submit(function(e){
+    $("#weight-form").submit(function (e) {
         e.preventDefault();
-        $.post($(this).attr('action'), $(this).serialize())
-            .done(function(data){
-                console.log('save done', data)
-            });
+        saveWeight($(this).attr('action'), $(this).serialize());
+    });
+
+    $(".weight-table .weight-value").editable({
+        url: '/save',
+        ajaxOptions: {
+            type: 'post',
+            dataType: 'json'
+        },
+        escape: false,
+        defaultValue: 0,
+        showbuttons: false,
+        onblur: 'submit'
     });
 });
--- weightmon.py
+++ weightmon.py
@@ -2,6 +2,7 @@ from orm import dbsession, Measurement
 from config import is_debug
 from flask import Flask, render_template, request, send_from_directory, jsonify, redirect
 from datetime import datetime
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 app.jinja_env.globals['now'] = datetime.now()
@@ -9,7 +10,6 @@ app.jinja_env.globals['now'] = datetime.now()
 
 def _calculate_diffs(measurements):
     for i, m in reversed(list(enumerate(measurements))):
-        print(i, m)
         if i == len(measurements)-1:
             m.diff = 0
         else:
@@ -18,11 +18,10 @@ def _calculate_diffs(measurements):
 
 @app.route('/save', methods=['POST'])
 def save_measurement():
-    print(request.headers)
     try:
-        date_val = datetime.strptime(request.form['d'], '%Y-%m-%d')
-        weight_val = float(request.form['v'])
-    except ValueError:
+        date_val = datetime.strptime(request.form.get('d', request.form.get('pk', '')), '%Y-%m-%d')
+        weight_val = float(request.form.get('v', request.form.get('value', '')))
+    except (ValueError, HTTPException):
         return jsonify(r='e') if request.headers.get('X-Requested-With', '') else redirect(request.referrer)
 
     m = dbsession.query(Measurement).filter(Measurement.measurement_date==date_val).first() or Measurement(measurement_date=date_val)
@@ -36,7 +35,7 @@ def save_measurement():
 def index():
     last_7_days = dbsession.query(Measurement).order_by(Measurement.measurement_date.desc()).limit(7).all()
     _calculate_diffs(last_7_days)
-    return render_template('index.html', last_7_days=last_7_days)
+    return render_template('index.html', last_7_days=last_7_days, total_diff=(last_7_days[0].value - last_7_days[-1].value))
 
 
 if __name__ == '__main__':
