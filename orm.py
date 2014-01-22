0d365c7117714750f7bd2fda284716f7b51fc5e6
Taylan Aydinli <taylanaydinli@gmail.com>
2014-01-22 11:50:34 +0200

    Added user class to tables

--- orm.py
+++ orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, DateTime, Float, Integer
+from sqlalchemy import create_engine, Column, DateTime, Float, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import initialize_config, is_debug
@@ -10,12 +10,23 @@ Base = declarative_base()
 engine = create_engine('postgresql://{DBUSER}:{DBPASS}@{DBSERVER}:{DBPORT}/{DBNAME}'.format(**environ),
                        echo=is_debug())
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer(), primary_key=True, nullable=False, autoincrement=True)
+    email = Column(String(), nullable=False)
+    password_hash = Column(String(), nullable=False)
+    password_salt = Column(String(), nullable=False)
+
+    def get_id(self):
+        return str(self.id)
+
 
 class Measurement(Base):
     __tablename__ = 'measurement'
     id = Column(Integer(), primary_key=True, nullable=False, autoincrement=True)
     measurement_date = Column(DateTime(), nullable=False, index=True)
     value = Column(Float(), nullable=False)
+    user_id = Column(Integer, nullable=False, ForeignKey('user.id'))
 
     def __str__(self):
         return '{0}: {1:.1f}kg'.format(self.measurement_date.strftime('%Y-%m-%d'), self.value)
