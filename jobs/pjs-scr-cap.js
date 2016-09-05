4014e9161136a78e29ff4b71eea1d44652b0a734
Taylan Aydinli <taylanaydinli@gmail.com>
2016-09-05 11:48:56 +0300

    Add delay to chart template so Google Charts js can load

--- jobs/pjs-scr-cap.js
+++ jobs/pjs-scr-cap.js
@@ -3,6 +3,8 @@ var args = require('system').args;
 var sourceFile = args[1];
 var destFile = args[2];
 page.open(sourceFile, function () {
-    page.render(destFile);
-    phantom.exit();
+    window.setTimeout(function() {
+        page.render(destFile);
+        phantom.exit();
+    }, 2000);
 });
